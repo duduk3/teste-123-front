a43dd3d1c8885309bf58f312d7fc8eb2ae81f19a
EDUARDO FERREIRA <dupearljam@gmail.com>
2022-07-25 10:53:16 -0300

    verifying error

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import './App.css';
 
 import React, { useEffect, useState } from 'react';
 
-API_POINT = 'https://hero-back-1978.herokuapp.com/users';
+// API_ENDPOINT = 'https://hero-back-1978.herokuapp.com/users';
 
 function App() {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch(API_POINT)
+    fetch(process.env.API_ENDPOINT)
       .then((res) => res.json())
       .then((parsedData) => { setData(parsedData); setLoading(false); });
   }, []);
@@ -17,24 +17,27 @@ function App() {
   if (loading) return <h1>loading...</h1>;
 
   return (
-    <table>
-      <tr>
-        {Object.keys(data[0]).map(
-          (key) => (<th key={ Math.random() }>{key}</th>),
-        )}
-      </tr>
-      <tbody>
-        {data.map((rowInfo) => (
-          <tr key={ Math.random() }>
-            {
-              Object.values(rowInfo).map(
-                (cellInfo) => (<td key={ Math.random() }>{cellInfo}</td>),
-              )
-            }
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <>
+      <h1>process.env.API_ENDPOINT</h1>
+      <table>
+        <tr>
+          {Object.keys(data[0]).map(
+            (key) => (<th key={ Math.random() }>{key}</th>),
+          )}
+        </tr>
+        <tbody>
+          {data.map((rowInfo) => (
+            <tr key={ Math.random() }>
+              {
+                Object.values(rowInfo).map(
+                  (cellInfo) => (<td key={ Math.random() }>{cellInfo}</td>),
+                )
+              }
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 }
 
